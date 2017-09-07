a9c612b3f4c13fe4fb1038ae15bc2db365e51166
chuchu1313 <fangyu1313@gmail.com>
2017-09-07 16:27:09 +0800

    Fix bug

--- ch3/Ex3_10/main.go
+++ ch3/Ex3_10/main.go
@@ -15,13 +15,14 @@ func main() {
 func comma(s string) string {
 	var buf bytes.Buffer
 	n := len(s)
-
+	preNum := n % 3
+	count := 0
 	for i := 0; i < n; i++ {
-		if i % 3 == 0 && i != 0 && i != n-1{
+		if (i == preNum && preNum != 0) || ((count-preNum) % 3 == 0  && count != n-1 && count != 0) {
 			buf.WriteString(",")
-		} else {
-			fmt.Fprintf(&buf, "%s", string(s[i]))
 		}
+		count++
+		fmt.Fprintf(&buf, "%s", string(s[i]))
 	}
 	return buf.String() 
 }
